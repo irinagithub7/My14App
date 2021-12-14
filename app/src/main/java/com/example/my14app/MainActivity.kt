2e8cc0a86ff1ebb90d178d315728bee931c51b77
unknown <irinagithub7>
2021-12-14 23:18:16 +0500

    Fragm

---
+++ app/src/main/java/com/example/my14app/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.my14app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigationMenu: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationMenu = findViewById(R.id.bottom_navigation_menu)
+
+        bottomNavigationMenu.setOnItemSelectedListener { item ->
+            var fragment: Fragment? = null
+            when (item.itemId) {
+                R.id.fragment_1 -> {
+                    fragment = FirstFragment()
+                }
+                R.id.fragment_2 -> {
+                    fragment = SecondFragment()
+                }
+            }
+            replaceFragment(fragment!!)
+            true
+        }
+        bottomNavigationMenu.selectedItemId = R.id.fragment_1
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(fragment.tag)
+            .commit()
+    }
+}
